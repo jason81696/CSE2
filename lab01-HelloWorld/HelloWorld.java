ed01e4b1db114a9ba8228c61aca842fe81bc4077
jason81696 <jss219@lehigh.edu>
2016-02-05 15:47:08 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+//
+//Jason Stein
+//Demo 0
+//Hello World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//     java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    public static void main (String[] args) {
+        System.out.println("#################################");
+        System.out.println("#################################");
+        System.out.println("#                               #");
+        System.out.println("#         Hello World 2016!     #");
+        System.out.println("#                               #");
+        System.out.println("#################################");
+        System.out.println("#################################");
+        System.out.println("#################################");
+        System.out.println("test");
+    }
+}
+        
